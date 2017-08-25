20de02e33636a24f5feb63a6e79744f76b230662
SAMUEL <sfverneck@gmail.com>
2017-08-25 00:09:37 -0300

    chore(src)

--- src/app.js
+++ src/app.js
@@ -5,35 +5,44 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+// carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // rota
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node API",
-        version: "0.0.1"
-    });
-});
+// const route = router.get('/', (req, res, next) => {
+//     res.status(200).send({
+//         title: "Node API",
+//         version: "0.0.1"
+//     });
+// });
+
 // rota
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({ 
-        id: id, 
-        item: req.body 
-    });
-});
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
+// const create = router.post('/', (req, res, next) => {
+//     res.status(201).send(req.body);
+// });
+// const put = router.put('/:id', (req, res, next) => {
+//     const id = req.params.id;
+//     res.status(201).send({ 
+//         id: id, 
+//         item: req.body 
+//     });
+// });
+// const del = router.delete('/', (req, res, next) => {
+//     res.status(200).send(req.body);
+// });
+
 // metodos
 // atribuiação da rota ao app
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+
+// app.use('/', route);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+// app.use('/products', put);
+// app.use('/products', del);
+
 // exportação da aplicação
 module.exports = app;
