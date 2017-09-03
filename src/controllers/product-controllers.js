94951496968c532b7c3ffb6394c9e33c65595571
SAMUEL <sfverneck@gmail.com>
2017-09-03 15:58:02 -0300

    chore(src)

--- src/controllers/product-controllers.js
+++ src/controllers/product-controllers.js
@@ -127,5 +127,17 @@ exports.put = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-    res.status(201).send(req.body);
+    // res.status(201).send(req.body);
+    Product
+    .findByIdAndRemove(req.body.id,) 
+    .then(x => {
+        res.status(200).send({
+            message: 'Produto removido com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+             message: 'Falha ao remover o produto',
+             date: e
+        });
+    });
 };
